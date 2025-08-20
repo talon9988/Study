16a3d4176bf4a7a0e40110ee762c239c2e82b2e6
J.J. <jiwonchoi993@gmail.com>
2025-08-20 19:47:04 +0900

    Add files via upload

---
+++ bookshop/AuthorUpdateTest.java
@@ -0,0 +1,58 @@
+package bookshop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AuthorUpdateTest {
+    public static void main(String[] args) {
+        // 0. import java.sql.*;  
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            // 1. JDBC 드라이버 (Oracle) 로딩
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // 2. Connection 얻어오기
+            String url = "jdbc:oracle:thin:@localhost:1521:xe";
+            conn = DriverManager.getConnection(url, "webdb", "webdb");
+            System.out.println("접속성공");
+
+            // 3. SQL문 준비 / 바인딩 / 실행
+            String query = "UPDATE author " + "SET author_name = ?," + "author_desc = ? " + "WHERE author_id = ?";
+
+            pstmt = conn.prepareStatement(query);
+
+            pstmt.setString(1, "이문열");
+            pstmt.setString(2, "삼국sdf지fsa작b가");
+            pstmt.setInt(3, 4);
+
+            int count = pstmt.executeUpdate();
+
+            // 4.결과처리
+            System.out.println(count + "건 처리");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("error: 드라이버 로딩 실패 - " + e);
+        } catch (SQLException e) {
+            System.out.println("error:" + e);
+        } finally {
+            // 5. 자원정리
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("error:" + e);
+            }
+
+        }
+
+    }
+
+}
---
+++ bookshop/Bookshop.java
@@ -0,0 +1,82 @@
+package bookshop;
+
+import java.util.List;
+
+public class Bookshop {
+
+	public static void main(String[] args) {
+		printAuthors();
+//		insertAuthor("정종욱", "뛰어난 AI 개발자");  
+//		updateAuthor(4L, "정종욱", "매우 뛰어난 AI 개발자(데이터 변경)");
+//		deleteAuthor(4L);
+		findAuthorById(3L);
+		findAuthorById(4L);
+		findAuthorById(5L);
+	}
+	
+	private static void findAuthorById(Long id) {
+		AuthorDAO dao = new AuthorDAOImpl();
+		AuthorVO authorVO = dao.get(id);
+		
+		if (authorVO == null) {
+			System.out.println("작가를 찾지 못했습니다.");
+		} else {
+			System.out.printf("작가 %s를 찾았습니다.%n", authorVO);
+		}
+	}
+	
+	private static void deleteAuthor(Long id) {
+		AuthorDAO dao = new AuthorDAOImpl();
+		boolean success = dao.delete(id);
+		
+		if (success) {
+			System.out.printf("작가 %d를 삭제하였습니다.%n", id);
+		} else {
+			System.out.printf("작가 %d를 삭제하지 못했습니다.%n", id);
+		}
+		printAuthors();
+	}
+	
+	private static void updateAuthor(Long id, String name, String desc) {
+		AuthorDAO dao = new AuthorDAOImpl();
+		AuthorVO authorVO = new AuthorVO(id, name, desc);
+		
+		boolean success = dao.update(authorVO);
+		
+		if (success) {
+			System.out.printf("저자 %s를 갱신하였습니다.%n", authorVO);
+		} else {
+			System.out.printf("저자 %s를 갱신하지 못했습니다.%n", authorVO);
+		}
+		printAuthors();
+	}
+	
+	private static void insertAuthor(String name, String desc) {
+		AuthorDAO dao = new AuthorDAOImpl();
+		AuthorVO authorVO = new AuthorVO(null, 
+				name, desc);
+		
+		boolean success = dao.insert(authorVO);
+		
+		if (success) {
+			System.out.printf("저자 %s를 추가했습니다.%n", 
+					authorVO);
+		} else {
+			System.out.printf("저자 %s를 추가하지 못했습니다.%n", 
+					authorVO);
+		}
+		
+		printAuthors();
+	}
+	
+	private static void printAuthors() {
+		AuthorDAO dao = new AuthorDAOImpl();
+		List<AuthorVO> list = dao.getList();
+		
+		System.out.println("===== 저자 목록 =====");
+		for (AuthorVO authorVO: list) {
+			System.out.println(authorVO);
+		}
+	}
+
+}
